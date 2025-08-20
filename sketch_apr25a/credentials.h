5d35adc9faa6f65351295b45370ed977b8f6d7ef
Swedlion <theo.lephay@gmail.com>
2025-08-20 23:08:49 +0200

    remove unused wifi pwd and ssid from credentials

--- sketch_apr25a/credentials.h
+++ sketch_apr25a/credentials.h
@@ -3,10 +3,6 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
-
-const char wifi_ssid[] = "your_ssid";
-const char wifi_password[] = "your_password";
-
 const char web_id[] = "bonjour";
 const char web_password[] = "ok";
 
